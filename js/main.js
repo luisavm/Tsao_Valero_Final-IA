4749f3538f2fcec3086da4a43c9a3e384263bded
SandraTsao <s_tsao@fanshaweonline.ca>
2019-04-09 15:02:38 -0400

    preload animaiton

--- js/main.js
+++ js/main.js
@@ -1,2 +1,32 @@
-// JavaScript Document
-console.log("Linked up");
+(() => {
+
+	console.log("Linked up");
+
+ 	let anchorLink = document.querySelector("#topButton"),
+		top = document.getElementById("mainHeader");
+
+	anchorLink.addEventListener("load", function() {
+		anchorLink.style.display = "block";
+	});
+
+	anchorLink.addEventListener("click", function(e) {
+    	if (window.scrollTo) {
+        e.preventDefault();
+        window.scrollTo({"behavior": "smooth", "top": top.offsetTop});
+    	}
+	});
+
+
+	const 	logoAni = document.querySelector("#logoAni");
+
+	if (sessionStorage.getItem("logoAnimated") === null) {
+		logoAni.classList.add("logoAni");
+	} else {
+		return;
+	}
+		setTimeout(function() {
+			logoAni.classList.remove("logoAni");
+			sessionStorage.setItem("logoAnimated", 1);
+		}, 4000);
+})();
+
